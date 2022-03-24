32ead78e6af303e91d880358799c5027834e65da
Sophie Verdier <100239319+SophieVerdier@users.noreply.github.com>
2022-03-24 17:04:39 +0100

    Create thanks.php

---
+++ thanks.php
@@ -0,0 +1,6 @@
+<?php
+
+echo 'Merci '.$name .' de nous avoir contacté à propos de '. $sujet. '<br/>';
+
+echo 'Un de nos conseillers vous contactera soit à l’adresse '. $email. ' ou par téléphone au '. $telephone. ' dans les plus brefs délais pour traiter votre demande : '.
+$message;
